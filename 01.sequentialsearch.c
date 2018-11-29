c9467ed336d6a1a9bc2693285d0f69c57c83ae2e
ceccs17d17 <43987627+ceccs17d17@users.noreply.github.com>
2018-11-29 21:49:10 +0530

    Update and rename 1.sequentialsearch.c to 01.sequentialsearch.c

--- 01.sequentialsearch.c
+++ 01.sequentialsearch.c
@@ -1,19 +1,26 @@
-//BAMISHA A
-//ROLL NO 17
+ /*
+1.SEQUENTIAL SEARCH
+NAME :BAMISHA A
+ROLL No. :17
+*/
 
 #include <stdio.h>
 
-int sequentialSearch(int array[], int size, int element) {
+int sequentialSearch(int array[], int size, int element)
+{
 	int i;
 
-	for (i=0; i<size; i++) {
-		if (array[i] == element) {
+	for (i=0; i<size; i++)
+	{
+		if (array[i] == element)
+		{
 			return (i+1);
 		}
 	}
 	return (0);
 }
-int main() {
+int main()
+ {
 	int i=0, found=0, array[100], size, element;
 
 	printf("Enter number of elements : ");
@@ -29,9 +36,24 @@ int main() {
 
 	found = sequentialSearch(array, size, element);
 
-	if (found) {
+	if (found) 
+	{
 		printf("Element found at position %d\n", found);
-	} else {
+	} 
+	else 
+	{
 		printf("Element not found\n");
 	}
 }
+
+    OUTPUT:
+	Enter the size 5
+	Enter elements
+	3
+	1
+	8
+	2
+	5
+	Enter number to be searched
+	8
+	8 is found in the list, at position 3
